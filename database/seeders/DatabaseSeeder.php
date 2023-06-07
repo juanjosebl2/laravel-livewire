3f743070fd6c0bef9bbd2880171eec3b0b024f27
juanjosebl2 <juanjosebl2@gmail.com>
2023-06-07 18:35:27 +0100

    added seed factory and migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //gender 7 files in DB
+        Task::factory(7)->create();
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,9 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <script src="https://cdn.tailwindcss.com"></script>
+        
+        @livewireStyles
+
     </head>
     <body>
         <section class="flex flex-col items-center space-y-4 py-12">
@@ -52,5 +55,8 @@
                 </tbody>
             </table>
         </section>
+        
+        @livewireScripts
+
     </body>
 </html>
